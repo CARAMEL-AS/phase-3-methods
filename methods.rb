4e4472ccfbdea7d1e81a4434885d1c9ea40edd3a
Aftab Sidhu <Aftab180@gmail.com>
2022-06-07 19:38:34 -0400

    commit tree 77a5beefaef6d0992e56daf61fb0709045484d6f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,24 @@
-# Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name = "Naureen")
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default(name = "Naureen")
+    puts "Hello, #{name}!"
+    p "Hello, programmer!"
+end
+
+def add(num1,num2)
+    p num1 + num2
+end
+
+def halve(num)
+   if num.is_a? String
+        return nil
+   else 
+        p num/2 
+   end
+end 
